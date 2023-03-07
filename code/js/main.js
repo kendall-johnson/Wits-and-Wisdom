d663711d4e41a364e1bb77a69e8104c389a34d01
kendall-johnson <kdotjohnson2019@gmail.com>
2023-03-07 11:10:31 -0800

    Add endGame Function and Fix Answer Error

--- code/js/main.js
+++ code/js/main.js
@@ -107,18 +107,21 @@
 /*----- state variables -----*/ // the two variables that come to mind that are necessary to track and update throughout the course of the game include the current score and the current question number. On a more robust iteration of the game, another state could be the topic of questions that a user selects, which I will attempt to implement, but it is not the main focus for the time being. 
 
 // set a variable to keep track of the current score
-let score;
+let score = 0;
+let randomQuestion;
+let answerEl;
+
 // set a variable for which question number the user is on
-let questionNum;
+// let questionNum;
 
 
 /*----- cached elements  -----*/
 
 // The cached elements will refer to elements within the HTML and reduces the amount of times the code calls a DOM query since it stores the result to a variable.
 // The HTML element that displays the current question.
-const currentQuestion = document.getElementById("question");
+const questionEl = document.getElementById("question"); //called in line 152
 // The HTML element that displays the answer choices.
-const currentChoices = document.getElementsByClassName("choices-container")
+const choicesEls = document.querySelectorAll('.choices-container button'); // setting choicesEls equal to all the buttons under the choices-container
 // The HTML element that displays the current score.
 const currentScore = document.getElementById("score");
 // The HTML element that will make a button function as a next question/skip question feature.
@@ -129,15 +132,22 @@ const skipQuestion = document.getElementById("skip-question");
 
   
 // Add a click event listener to the answer options, so that when a user clicks an answer it will go to the next question. (We also want to display whether that answer was right or wrong with a delay before switching to the next question)
-// document.querySelectorAll('#choice-1, #choice-2, #choice-3, #choice-4').addEventListener('click', handleChoice);
+const buttons = document.querySelectorAll('.choices-container button');
+buttons.forEach(button => {
+  button.addEventListener('click', handleChoice);
+})
 // Add a similar click event listener to the 'next question/skip question' button so that it listens for a click then immediately skips to the next question.
-// document.querySelector('#skip-question').addEventListener('click', nextQuestion);
+skipQuestion.addEventListener('click', nextQuestion);
   
 /*----- functions -----*/ // remember to use function render() and to keep it concise by calling other functions within the render. 
-init();
-// Define a function to render the current question and answer options.
-function init() {
- // generate a random index number between 0 and the length of the `questions` array
+function startGame() {
+  score = 0;
+  renderScore(score);
+  render();
+}
+// create a function to generate a random question and its accompanying choices on the webpage
+function nextQuestion() {
+  // generate a random index number between 0 and the length of the `questions` array
   const randomIdx = Math.floor(Math.random()* questions.length);
   // access the question object at the random index number provided above
   const randomQuestion = questions[randomIdx];
@@ -145,22 +155,64 @@ function init() {
   const currentQuestion = randomQuestion.question;
   const choices = randomQuestion.choices;
   // render the question text and choices as HTML elements using DOM manipulation
-  const questionEl = document.getElementById('question');
   questionEl.innerHTML = currentQuestion; // changing what is inside the ID 'question' in the HTML to the value of the randomly selected question text.
-  
-  const choicesEls = document.querySelectorAll('.choices-container button'); // setting choicesEls equal to all the buttons under the choices-container
+  // initiate a for loop to generate the choices that go along with the randomly generated question into their necessary buttons
   for (let i = 0; i < choices.length; i++) { // it then iterates through all of those buttons
     const choiceEl = choicesEls[i]; // sets choiceEl equal to the specific choice button at a given time
     choiceEl.innerHTML = choices[i]; // set the inside of the specific button choice equal to the necessary potential answer choice from the original constant array.
   }
-
-  // render score
-
-  // call render()
+  answerEl = randomQuestion.answer;
 }
-// Define a function to handle the user's answer and update the score and current question index. We also want to implement the ability to display whether or not the answer was right within this function as well. 
+
+// create a function that takes note of the users choice
+function handleChoice(evt) {
+  const selectedAnswer = evt.target.textContent; // target is a read-only property, textContent returns the string of the selected button in this case.
+  checkAnswer(selectedAnswer);
+  render()
+}
+
+// create a function that checks if the users choice was the correct answer
+function checkAnswer(selectedAnswer) { // takes the parameter answer as an input and checks if it is the correct one
+  if (selectedAnswer === answerEl) {
+    score++; // if it is correct, it adds one to the score
+  }
+  renderScore(score); // then it renders the new updated score accordingly
+}
+
+// create a function that renders the score
+function renderScore(score) {
+  currentScore.innerHTML = `Score: ${score}`; // renders "Score: (whatever current score is based on number of correct answers"
+}
 
 // Define a function to end the game and display the final score.
-function render() {
+function endGame() {
+  questionEl.innerHTML = "CONGRATULATIONS! You have won Wits & Wisdom!";
+  // for (let i=0; i < choicesEls.length; i++) {
+  //   choicesEls.style.visibility = "hidden";
+  // }
+  renderScore(score);
 
+  // skipQuestion.remove();
+  // implement restart function upon click of new button
+  const restartBtn = document.createElement("button");
+  restartBtn.innerHTML = "Play Again!";
+  restartBtn.classList.add("restart-button")
+  restartBtn.addEventListener('click', startGame);
+  // add restart button to HTML document
+  document.body.append(restartBtn);
+  // remove the restart button once it is clicked
+  restartBtn.addEventListener('click', function() {
+    restartBtn.remove();
+  })
 }
+// define a render function that displays all results of above functions if necessary
+function render() {
+  if (score < 2) {
+    nextQuestion();
+    renderScore(score);
+  } else {
+    endGame();
+  }
+}
+
+render()
